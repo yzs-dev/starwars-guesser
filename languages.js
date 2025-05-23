7b7fdf8c0b7b96c8be535881478c03075347257a
And3rson49 <jayananderson13@outlook.com>
2025-05-23 23:01:53 +0530

    Files upload

---
+++ languages.js
@@ -0,0 +1,52 @@
+export const languages = [
+    {
+        name: "Luke Skywalker",
+        backgroundColor: "#4A90E2", // Blue lightsaber
+        color: "#F9F4DA",
+    },
+    {
+        name: "Darth Vader",
+        backgroundColor: "#000000", // Black armor
+        color: "#FF0000", // Red lightsaber
+    },
+    {
+        name: "Yoda",
+        backgroundColor: "#8BC34A", // Green skin
+        color: "#F9F4DA",
+    },
+    {
+        name: "Leia Organa",
+        backgroundColor: "#FFFFFF", // White outfit
+        color: "#000000",
+    },
+    {
+        name: "Han Solo",
+        backgroundColor: "#6D4C41", // Brown jacket
+        color: "#F9F4DA",
+    },
+    {
+        name: "Obi-Wan Kenobi",
+        backgroundColor: "#B57F4F", // Jedi robes
+        color: "#F9F4DA",
+    },
+    {
+        name: "Chewbacca",
+        backgroundColor: "#5D4037", // Brown fur
+        color: "#F9F4DA",
+    },
+    {
+        name: "R2-D2",
+        backgroundColor: "#4A90E2", // Blue and white
+        color: "#FFFFFF",
+    },
+    {
+        name: "C-3PO",
+        backgroundColor: "#FFD700", // Gold plating
+        color: "#000000",
+    },
+    {
+        name: "Emperor Palpatine",
+        backgroundColor: "#2D2D2D", // Dark robes
+        color: "#FF0000", // Sith red
+    },
+];
